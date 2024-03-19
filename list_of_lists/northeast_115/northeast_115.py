74a35426a4f79b1b0d588b02677b07b2c2b87d03
veranavera <veronica.e.r.ashcroft@gmail.com>
2024-03-19 16:33:00 -0400

    Winter trip reports

---
+++ list_of_lists/northeast_115/northeast_115.py
@@ -0,0 +1,83 @@
+import csv
+import operator
+sorts = ["name", "elevation", "prominence", "isolation", "date", "location"]
+
+links = [sorts.copy(), sorts.copy(), sorts.copy(), sorts.copy(), sorts.copy(), sorts.copy()]
+for x in range(0, 6):
+    for y in range(0,6):
+        if(x == y and not "." in links[x][y]):
+            links[x][y] = "northeast_115_" + links[x][y] + "_descending.html"
+        elif(not "." in links[x][y]):
+            links[x][y] = "northeast_115_" + links[x][y] + ".html"
+
+sortCounters = [0, 2, 3, 4, 5, 7]
+sortOrders = [False, True, True, True, True, False]
+sortTypes = [False, True, True, True, False, False]
+sortCounter = 0
+
+for type in sorts:
+    header = "<!DOCTYPE html>\n<html>\n<head>\n\t<link rel=\"stylesheet\" href=\"stylesheet.css\">\n\t<title>Peaks I've climbed!!!</title>\n</head>\n<body>\n<div>\n\t<iframe class=\"page_header\" frameBorder=\"0\" src=\"../../formatting_files/header.html\" seamless></iframe>\n</div>\n<div class=\"page_body\">\n\t<h1>The Northeast 115</h1>\n\t<p>\n\t\tThere is little I can say about this iconic list that hasn't already been said, as this is probably one of the most popular lists in the world of it's size. From the Appalachian plateaus of the Catskills to Katahdin's craggy summits, this list contains the tallest mountains in the Northeast - including the high points of New York, Vermont, New Hampshire, and Maine. This was my first major peakbagging project, and it took me around three years to complete from when I first climbed Carrigan - and only 115 days from when I started with taking a trans pride flag up Tecumseh. Suffice to say, this list has a special place in my heart, and I will know all of these mountains by heart for years and years to come\n\t</p>\n\t<p>\n\t\t Below are all the peaks on this list, with each peak linked to its page on peakbagger, and with the elevation, prominence, isolation, and location pulled from that website. Also, each peak has the date I climbed it, which may eventually turn into a \"link to the trip report page\" type deal, but as of March 11th, 2024, I'm too lazy to do all that work (for the time being, you can click the \"trip reports\" link in the header and find the ascent by date). Finally, note that I try and use an indigenous name for a mountain whenever I have that data available, so oftentimes the linked peak will show up under a different, colonial name. If you know any other indigenous names for any of these mountains, let me know and I will happy correct their names!!!\n\t</p>\n\t<p>\n\t\tYou can feel free click on each of the table headers and get a list sorted by that parameter (isolation, date, etc.) - the default is prominence, but the list also looks really cool from other perspectives!!!\n\t</p>\n"
+
+    footer = "\n\t</table>\n</div>\n<div>\n\t<iframe class=\"page_footer\" frameBorder=\"0\" src=\"../../formatting_files/footer.html\" seamless></iframe>\n</div>\n</body>"
+
+    htmlData = header + "</div>\n<div style=\"width: 100%; display: table;\">\n\t<table style=\"width:100%\">\n\t\t<tr>\n\t\t\t<th class=\"number\"><a href=\"" + links[sortCounter][2] + "\" class=\"number\">Number &#8645</a></th>\n\t\t\t<th><a href=\"" + links[sortCounter][0] + "\" class=\"name\">Name &#8645</a></th>\n\t\t\t<th><a href=\"" + links[sortCounter][1] + "\" class=\"elevation\">Elevation (ft) &#8645</a></th>\n\t\t\t<th><a href=\"" + links[sortCounter][2] + "\" class=\"prominence\">Prominence (ft) &#8645</a></th>\n\t\t\t<th><a href=\"" + links[sortCounter][3] + "\" class=\"isolation\">Isolation (km) &#8645</a></th>\n\t\t\t<th><a href=\"" + links[sortCounter][4] + "\" class=\"date\">Date Climbed &#8645</a></th>\n\t\t\t<th><a href=\"" + links[sortCounter][5] + "\" class=\"location\">State &#8645</a></th>\n\t\t"
+    
+    with open("northeast_115.csv", newline='') as csvfile:
+        data = csv.reader(csvfile, delimiter=',', quotechar='|')
+        next(data)
+        if(sortTypes[sortCounter]):
+            sortedData = sorted(data, key=lambda x: float(x[sortCounters[sortCounter]]), reverse=sortOrders[sortCounter])
+        else:
+            sortedData = sorted(data, key=operator.itemgetter(sortCounters[sortCounter]), reverse=sortOrders[sortCounter])
+        counter = 1
+        for row in sortedData:
+            line = "<tr>\n\t\t\t"
+            line += "<th class = \"number\">" + str(counter) + "</th>\n\t\t\t"
+            line += "<th class = \"name\"><a href=\"" + row[1] + "\">" + row[0] + "</a></th>\n\t\t\t"
+            line += "<th class = \"elevation\">" + str(row[2]) + "</th>\n\t\t\t"
+            line += "<th class = \"prominence\">" + str(row[3]) + "</th>\n\t\t\t"
+            line += "<th class = \"isolation\">" + str(row[4]) + "</th>\n\t\t\t"
+            line += "<th class = \"date\">" + row[5] + "</th>\n\t\t\t"
+            #line += "<th class = \"date\"><a href=\"" + row[6] + "\">" + row[5] + "</a></th>\n\t\t\t"
+            line += "<th class = \"location\">" + row[7] + "</th>\n\t\t\t"
+            line += "</tr>\n\t\t"
+            htmlData += line
+            counter += 1
+            
+    htmlData += footer
+
+    f = open("northeast_115_" + type + ".html", "w")
+    f.write(htmlData)
+    f.close()
+
+    htmlData = header + "</div>\n<div style=\"width: 100%; display: table;\">\n\t<table style=\"width:100%\">\n\t\t<tr>\n\t\t\t<th class=\"number\"><a href=\"northeast_115_prominence.html\" class=\"number\">Number &#8645</a></th>\n\t\t\t<th><a href=\"northeast_115_name.html\" class=\"name\">Name &#8645</a></th>\n\t\t\t<th><a href=\"northeast_115_elevation.html\" class=\"elevation\">Elevation (ft) &#8645</a></th>\n\t\t\t<th><a href=\"northeast_115_prominence.html\" class=\"prominence\">Prominence (ft) &#8645</a></th>\n\t\t\t<th><a href=\"northeast_115_isolation.html\" class=\"isolation\">Isolation (km) &#8645</a></th>\n\t\t\t<th><a href=\"northeast_115_date.html\" class=\"date\">Date Climbed &#8645</a></th>\n\t\t\t<th><a href=\"northeast_115_location.html\" class=\"location\">State &#8645</a></th>\n\t\t"
+    
+    with open("northeast_115.csv", newline='') as csvfile:
+        data = csv.reader(csvfile, delimiter=',', quotechar='|')
+        next(data)
+        if(sortTypes[sortCounter]):
+            sortedData = sorted(data, key=lambda x: float(x[sortCounters[sortCounter]]), reverse=not sortOrders[sortCounter])
+        else:
+            sortedData = sorted(data, key=operator.itemgetter(sortCounters[sortCounter]), reverse=not sortOrders[sortCounter])
+        counter = 1
+        for row in sortedData:
+            line = "<tr>\n\t\t\t"
+            line += "<th class = \"number\">" + str(counter) + "</th>\n\t\t\t"
+            line += "<th class = \"name\"><a href=\"" + row[1] + "\">" + row[0] + "</a></th>\n\t\t\t"
+            line += "<th class = \"elevation\">" + str(row[2]) + "</th>\n\t\t\t"
+            line += "<th class = \"prominence\">" + str(row[3]) + "</th>\n\t\t\t"
+            line += "<th class = \"isolation\">" + str(row[4]) + "</th>\n\t\t\t"
+            line += "<th class = \"date\">" + row[5] + "</th>\n\t\t\t"
+            #line += "<th class = \"date\"><a href=\"" + row[6] + "\">" + row[5] + "</a></th>\n\t\t\t"
+            line += "<th class = \"location\">" + row[7] + "</th>\n\t\t\t"
+            line += "</tr>\n\t\t"
+            htmlData += line
+            counter += 1
+            
+    htmlData += footer
+
+    f = open("northeast_115_" + type + "_descending.html", "w")
+    f.write(htmlData)
+    f.close()
+
+    sortCounter += 1
